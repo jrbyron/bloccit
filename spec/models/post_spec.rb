3080f341643cc63db07423d3b3a096895ef95a35
Joel Byron <jrbyron@gmail.com>
2014-10-26 04:19:01 -0400

    downvote

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,6 +17,12 @@ require 'rails_helper'
          expect( @post.up_votes ).to eq(3)
        end
      end
+
+     describe '#down_votes' do
+       it "counts the number of votes with value = -1" do
+        expect( @post.down_votes ).to eq(2)
+       end
+     end
  
      describe '#points' do
        it "returns the sum of all down and up votes" do
